9f0c64503629e6db1928aa6e0b4119b216ac225c
jawad30 <jawadakabour30@gmail.com>
2025-01-20 21:23:41 +0100

    updates

--- htdocs/php/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "date 1: ".date("d/M/Y")."</br>";
-// echo "date 2: ".date("d-M-Y")."</br>";
-// echo "date 3: ".date("d.M.Y")."</br>";
--- htdocs/php/otherside.php
+++
@@ -1,4 +0,0 @@
-<?php
-include "menu.php";
-// require "menu.php"; stop l'execusion
-// require_once "menu.php";  force to not repeat for > one more tme 
